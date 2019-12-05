f74c421acc5a40414c4bbf270e2c62f62a5b977b
xiaofeizhang19 <xzhang613@gmail.com>
2019-12-05 15:51:07 +0000

    Add route reponds to POST request

--- api/app.rb
+++ api/app.rb
@@ -1,8 +1,22 @@
 require 'sinatra/base'
+require_relative 'lib/url_shortener'
 
 class UrlManager < Sinatra::Base
-  get '/test' do
-    'Hello World'
+  before do
+    @url_shortener = UrlShortener.new
+  end
+q
+  get '/:short_url' do
+    # Retrive original url and redirect
+  end
+
+  post '/' do
+    original_url = JSON.parse(request.body.read)["url"]
+    short_url = @url_shortener.add(original_url)
+    {
+      "short_url": "/#{short_url}",
+      "url": original_url
+    }.to_json
   end
 
   run if app_file == $0
