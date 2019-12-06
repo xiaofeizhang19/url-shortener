231ff3c200373332454d16f9150f73123d159a63
xiaofeizhang19 <xzhang613@gmail.com>
2019-12-06 12:13:22 +0000

    Add functions for sending request and getting response from backend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,18 +7,38 @@ export default class App extends Component {
       originalUrl: '',
       shortUrl:''
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({originalUrl: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { originalUrl } = this.state;
+    fetch('http://localhost:4567', {
+      method: 'POST',
+      body: JSON.stringify({url: originalUrl})
+    })
+      .then(response => response.json())
+      .then(data => {this.setState({shortUrl: data.short_url})})
+      .catch(error => alert(error));
   }
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Input the URL to be shortened:
             <br />
             <input 
               type="text"
               value={this.state.originalUrl}
+              onChange={this.handleChange}
             />
           </label>
           <input type="submit" value="Submit" />
