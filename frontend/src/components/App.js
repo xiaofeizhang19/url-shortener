1643b03f0ba44d2cc2e38fa99b32834d949108e3
xiaofeizhang19 <xzhang613@gmail.com>
2019-12-06 01:28:38 +0000

    Add text input and submit button

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,29 @@
 import React, { Component }from 'react'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      originalUrl: '',
+      shortUrl:''
+    }
+  }
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <form>
+          <label>
+            Input the URL to be shortened:
+            <br />
+            <input 
+              type="text"
+              value={this.state.originalUrl}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
